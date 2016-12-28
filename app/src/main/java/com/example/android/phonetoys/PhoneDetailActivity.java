33cf79754769f04601ebf2c93c734fed4592862b
wimbet <wimbet@gmail.com>
2016-12-28 14:44:18 -0600

    Updated PhoneDetailActivity

--- app/src/main/java/com/example/android/phonetoys/PhoneDetailActivity.java
+++ app/src/main/java/com/example/android/phonetoys/PhoneDetailActivity.java
@@ -2,8 +2,10 @@ package com.example.android.phonetoys;
 
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,23 +15,46 @@ import android.widget.TextView;
 
 import com.example.android.phonetoys.data.PhoneContract.PhoneEntry;
 
+import org.w3c.dom.Text;
+
 public class PhoneDetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     /** Identifier for the pet data loader */
     private static final int PHONE_LOADER = 0;
 
+    /** Content URI for the existing phone) */
+    private Uri mCurrentPhoneUri;
+
     /** Adapter for the ListView */
     PhoneCursorAdapter mCursorAdapter;
+
+    /** TextView field to display the phone's name */
+    private TextView mNameTextView;
+
+    /** TextView field to display the phone's quantity */
+    private TextView mQuantityTextView;
+
+    /** TextView field to display the phone's price */
+    private TextView mPriceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_detail);
 
+        // Examine the intent that was used to launch this activity
+        Intent intent = getIntent();
+        mCurrentPhoneUri = intent.getData();
+
         // Find the TextViews which will be populated with the phone data
         TextView nameTextView = (TextView) findViewById(R.id.display_phone_name);
         TextView quantityTextView = (TextView) findViewById(R.id.display_phone_quantity);
         TextView priceTextView = (TextView) findViewById(R.id.display_phone_price);
+
+        //assign the textviews
+        nameTextView = mNameTextView;
+        quantityTextView = mQuantityTextView;
+        priceTextView = mPriceTextView;
 
         // Setup an Adapter to create a list item for each row of phone data in the Cursor.
         // There is no pet data yet (until the loader finishes) so pass in null for the Cursor.
@@ -60,9 +85,26 @@ public class PhoneDetailActivity extends AppCompatActivity implements LoaderMana
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        // Update {@link PhoneCursorAdapter} with this new cursor containing updated phone data
-        mCursorAdapter.swapCursor(data);
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Proceed with moving to the first row of the cursor and reading data from it
+        // (This should be the only row in the cursor)
+        if (cursor.moveToFirst()) {
+            // Find the columns of phone attributes that we're interested in
+            int nameColumnIndex = cursor.getColumnIndex(PhoneEntry.COLUMN_PHONE_NAME);
+            int quantityColumnIndex = cursor.getColumnIndex(PhoneEntry.COLUMN_PHONE_QUANTITY);
+            int priceColumnIndex = cursor.getColumnIndex(PhoneEntry.COLUMN_PHONE_PRICE);
+
+            // Extract out the value from the Cursor for the given column index
+            String name = cursor.getString(nameColumnIndex);
+            int quantity = cursor.getInt(quantityColumnIndex);
+            int price = cursor.getInt(priceColumnIndex);
+
+            // Update the views on the screen with the values from the database
+            mNameTextView.setText(name);
+            mQuantityTextView.setText(quantity);
+            mPriceTextView.setText(price);
+        }
+
     }
 
     @Override
