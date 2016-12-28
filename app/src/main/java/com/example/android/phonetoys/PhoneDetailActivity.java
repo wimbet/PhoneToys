355f4398c65f969c991bed0ef96cd35280e3d604
wimbet <wimbet@gmail.com>
2016-12-28 14:18:41 -0600

    Started PhoneDetailActivity

--- app/src/main/java/com/example/android/phonetoys/PhoneDetailActivity.java
+++ app/src/main/java/com/example/android/phonetoys/PhoneDetailActivity.java
@@ -1,13 +1,73 @@
 package com.example.android.phonetoys;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class PhoneDetailActivity extends AppCompatActivity {
+import com.example.android.phonetoys.data.PhoneContract.PhoneEntry;
+
+public class PhoneDetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    /** Identifier for the pet data loader */
+    private static final int PHONE_LOADER = 0;
+
+    /** Adapter for the ListView */
+    PhoneCursorAdapter mCursorAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_detail);
+
+        // Find the TextViews which will be populated with the phone data
+        TextView nameTextView = (TextView) findViewById(R.id.display_phone_name);
+        TextView quantityTextView = (TextView) findViewById(R.id.display_phone_quantity);
+        TextView priceTextView = (TextView) findViewById(R.id.display_phone_price);
+
+        // Setup an Adapter to create a list item for each row of phone data in the Cursor.
+        // There is no pet data yet (until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new PhoneCursorAdapter(this, null);
+        //nameTextView.setAdapter(mCursorAdapter);
+
+        // Kick off the loader
+        getLoaderManager().initLoader(PHONE_LOADER, null, this);
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                PhoneEntry._ID,
+                PhoneEntry.COLUMN_PHONE_NAME,
+                PhoneEntry.COLUMN_PHONE_QUANTITY,
+                PhoneEntry.COLUMN_PHONE_PRICE };
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                PhoneEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Update {@link PhoneCursorAdapter} with this new cursor containing updated phone data
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
     }
 }
